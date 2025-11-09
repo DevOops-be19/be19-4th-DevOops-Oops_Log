b1feef08ed53aee2dda01dd2240657ff1ecdd0c4
DESKTOP-9FU9MPH\정건일 <kyuhon123@gmail.com>
2025-11-09 16:12:27 +0900

    feat: #5 관리자 페이지 레이아웃 진행중

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import adminRoutes from './modules/admin.routes';
+
+const routes = [
+  ...adminRoutes, 
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  history: createWebHistory(),
+  routes,
+});
 
 export default router
---
+++ frontend/src/views/admin/components_1/Sidebar.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="sidebar">
+        <h1>oops_log</h1>
+        <nav class="menu">
+            <RouterLink to="/admin/member" class="menu-item">회원조회</RouterLink>
+            <RouterLink to="/admin/report" class="menu-item">신고</RouterLink>
+            <RouterLink to="/admin/tag" class="menu-item">태그</RouterLink>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.sidebar {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* 전체 중앙 정렬 */
+  justify-content: start;
+  width: 100%;
+  height: 100vh;
+  background-color: #2f3b52;
+  color: white;
+  padding-top: 2rem;
+  box-sizing: border-box;
+}
+
+/* 로고 영역 */
+.logo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  margin-bottom: 2rem;
+}
+
+.logo img {
+  width: 60px;
+  height: 60px;
+  object-fit: contain;
+  border-radius: 50%;
+}
+
+/* 메뉴 영역 */
+.menu {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+  align-items: center; /* 메뉴도 중앙 정렬 */
+  padding: 0 1rem;
+}
+
+.menu-item {
+  display: block;
+  width: 95%;
+  margin-top: 10px;
+  padding: 4px;
+  text-align: center;
+  text-decoration: none; /* 밑줄 제거 */
+  background-color: #3f4b65;
+  color: white;
+  border-radius: 6px;
+  font-weight: 500;
+  transition: all 0.2s ease;
+}
+
+.menu-item:hover {
+  background-color: #556080;
+  transform: scale(1.03);
+}
+</style>
