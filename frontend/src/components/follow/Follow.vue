c61cddd74e329d414cdab52498c5ae669e1bc4c9
SoonWooLee1 <lsw010420@gmail.com>
2025-11-13 16:50:02 +0900

    팔로우 북마크 오류 수정

--- frontend/src/components/follow/Follow.vue
+++ frontend/src/components/follow/Follow.vue
@@ -107,8 +107,8 @@ function navigateToDetail(item) {
   if (recordType === 'ooh') {
     router.push({ name: 'DetailOoh', params: { id: recordId } });
   } else if (recordType === 'oops') {
-    // router.push({ name: 'DetailOops', params: { id: recordId } });
-    toastStore.showToast('Oops 상세 페이지는 현재 준비 중입니다.');
+    router.push({ name: 'DetailOops', params: { id: recordId } });
+    // toastStore.showToast('Oops 상세 페이지는 현재 준비 중입니다.');
   }
 }
 
